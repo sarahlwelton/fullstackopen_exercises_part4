f7c95a72eac730b2966876d5dd0e577c31916406
Sarah Welton <sarah.welton@couchbase.com>
2025-01-29 09:58:46 -0500

    Exercise 4.6 helper functions and unit tests Step 4: Define a function called mostBlogs that receives an array of blogs and returns the author who has the largest amount of blogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,5 @@
 const dummy = (blogs) => {
-  console.log(blogs)
+  // console.log(blogs)
   return 1
 }
 
@@ -21,6 +21,48 @@ const favoriteBlog = (blogs) => {
     : sortedBlogs[sortedBlogs.length - 1]
 }
 
+const countAuthors = (blogs) => {
+
+  const authors = blogs.map((blog) => blog.author)
+
+  const createCount = (authors) => {
+
+    let authorCount = {}
+
+    authors.forEach(author => {
+
+      if (authorCount[author] === undefined ) {
+        authorCount[author] = 1
+      } else {
+        authorCount[author] = authorCount[author] + 1
+      }
+    })
+    console.log(authorCount)
+    return authorCount
+  }
+
+  return createCount(authors)
+}
+
+const mostBlogs = (blogs) => {
+
+  const countedAuthors = countAuthors(blogs)
+
+  let highestCount = 0
+
+  let maxAuthor = ''
+
+  Object.entries(countedAuthors).forEach(([ author, count ]) => {
+    if (count > highestCount) {
+      highestCount = count
+      maxAuthor = author
+    }
+  })
+  return highestCount === 0
+    ? undefined
+    :{ 'author': maxAuthor, 'blogs': highestCount }
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs, countAuthors
 }
