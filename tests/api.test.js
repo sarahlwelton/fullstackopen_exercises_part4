2764efa581924d2f50eed5ac864a2968d19f6d6b
Sarah Welton <sarah.welton@couchbase.com>
2025-02-04 12:05:06 -0500

    Exercise 4.9 blog list tests Step 2: Write a test that verifies that the unique identifier property of the blog posts is named id.
    (Recommended changes for defining id parameter were done in a previous exercise)

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,16 +1,23 @@
-const { test, after, describe } = require('node:test')
+const { test, after } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 
-const Blog = require('../models/blog')
-
 test('the list of all blogs is returned correctly', async () => {
   const response = await api.get('/api/blogs')
 
   assert.strictEqual(response.body.length, 6)
+})
+
+test('the unique ID property for each blog is named "id"', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.forEach(blog => {
+    assert.match(blog.id, /^[0-9a-f]{24}$/i)
+  })
+
 })
 
 after(async () => {
