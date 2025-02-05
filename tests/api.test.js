da4a4ffb53f7c713907f15c9f58de282a6c78f6e
Sarah Welton <sarah.welton@couchbase.com>
2025-02-05 11:24:46 -0500

    Exercise 4.10 blog list tests Step 3: Write a test that verifies that making an HTTP POST request to the /api/blogs URL successfully creates a new blog post.
    Refactor to use async/await

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,4 +1,5 @@
 const { test, after } = require('node:test')
+const helper = require('../utils/list_helper')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -18,6 +19,30 @@ test('the unique ID property for each blog is named "id"', async () => {
     assert.match(blog.id, /^[0-9a-f]{24}$/i)
   })
 
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Adventures in React',
+    author: 'Mary Poppins',
+    url: 'https://test.test.com/',
+    likes: 0
+  }
+
+  const initialBlogsResult = await helper.initialBlogs()
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+
+  assert.strictEqual(blogsAtEnd.body.length, initialBlogsResult.length + 1)
+
+  const blogContents = blogsAtEnd.body.map(blog => blog.title)
+  assert(blogContents.includes('Adventures in React'))
 })
 
 after(async () => {
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,7 @@
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
 const dummy = (blogs) => {
   return 1
 }
@@ -92,6 +96,12 @@ const mostLikes = (blogs) => {
     :{ 'author': maxAuthor, 'likes': highestLikes }
 }
 
+const initialBlogs = async () => {
+  const response = await api.get('/api/blogs')
+
+  return response.body
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog, mostBlogs, countAuthors, countLikes, mostLikes
+  dummy, totalLikes, favoriteBlog, mostBlogs, countAuthors, countLikes, mostLikes, initialBlogs
 }
