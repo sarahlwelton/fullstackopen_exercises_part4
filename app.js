bae2eba74f5c1018ebed419282a352f47bad3470
Sarah Welton <sarah.welton@couchbase.com>
2025-02-10 17:13:05 -0500

    Exercise 4.15 blog list expansion Step 3: Implement a way to create new users via HTTP POST. Users must have a username, name, and password. Use bycrypt. List all users with a suitable HTTP request.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/error_middleware')
 const config = require('./utils/config')
 
@@ -15,6 +16,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
