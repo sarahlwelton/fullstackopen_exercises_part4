f49a3973ca384f2e7c7f4cb0903518d12d82f55a
Sarah Welton <sarah.welton@couchbase.com>
2025-02-12 10:59:06 -0500

    Exercise 4.20 blog list expansion Step 8: Extract the token helper function to a middleware

---
+++ utils/token_extractor.js
@@ -0,0 +1,10 @@
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('Authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+
+  next()
+}
+
+module.exports = tokenExtractor
