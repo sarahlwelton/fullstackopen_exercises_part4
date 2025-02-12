ec9ef2693fd6968d46f69b3eaf8f82f5e3d4e64e
Sarah Welton <sarah.welton@couchbase.com>
2025-02-12 15:39:00 -0500

    Working commit - Create middleware for extracting user info from JWT

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -11,13 +11,7 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'Invalid token' })
-  }
-
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blog = new Blog({
     title: body.title,
--- utils/token_extractor.js
+++ utils/token_extractor.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('Authorization')
   if (authorization && authorization.startsWith('Bearer ')) {
@@ -7,4 +10,18 @@ const tokenExtractor = (request, response, next) => {
   next()
 }
 
-module.exports = tokenExtractor
+const getUser = async (decodedToken) => {
+  return await User.findById(decodedToken.id)
+}
+
+const userExtractor = (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    response.status(401).json({ error: 'Invalid token' })
+  } else {
+    request.user = getUser(decodedToken)
+  }
+  next()
+}
+
+module.exports = { tokenExtractor, userExtractor }
